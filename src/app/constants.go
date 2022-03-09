95c10f64f70d3d7c099733ce130902d666de11eb
Robin <robin1998@hotmail.no>
2022-03-09 11:24:49 +0100

    added constants to be used

--- src/app/constants.go
+++ src/app/constants.go
@@ -1,1 +1,10 @@
 package constants
+
+const VERSION = "v1"
+const DEFAULT_PATH = "/"
+const STATUS_PATH = "/corona/v1/status/"
+const POLICY_PATH = "/corona/v1/policy/"
+const CASES_PATH = "/corona/v1/cases/"
+const NOTIFICATION_PATH = "/corona/v1/notifications/"
+const COVID_CASES_API = "https://github.com/rlindskog/covid19-graphql"
+const CORONA_POLICY_API = "https://covidtracker.bsg.ox.ac.uk/about-api"
