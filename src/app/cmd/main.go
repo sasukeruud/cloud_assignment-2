941bae333076fbdc79ef8bd651b18b0926d09089
Robin <robin1998@hotmail.no>
2022-03-09 11:36:29 +0100

    added running code to main, no functions implemented

--- src/app/cmd/main.go
+++ src/app/cmd/main.go
@@ -1,1 +1,19 @@
 package main
+
+import (
+	constants "assignment_2/src/app"
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	fmt.Println("Running")
+	fmt.Print(constants.VERSION)
+
+	//http.HandleFunc(constants.DEFAULT_PATH, )
+	//http.HandleFunc(constants.CASES_PATH)
+	//http.HandleFunc(constants.POLICY_PATH, )
+	//http.HandleFunc(constants.STATUS_PATH, )
+	//http.HandleFunc(constants.NOTIFICATION_PATH, )
+	http.ListenAndServe(":8080", nil)
+}
