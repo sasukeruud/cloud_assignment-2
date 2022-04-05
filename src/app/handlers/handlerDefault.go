ba28304920ef53100d9dd376584745f931e14e3d
Robin <robin1998@hotmail.no>
2022-04-05 21:10:43 +0200

    added status code

--- src/app/handlers/handlerDefault.go
+++ src/app/handlers/handlerDefault.go
@@ -2,7 +2,6 @@ package handlers
 
 import (
 	constants "assignment_2/src/app"
-	"fmt"
 	"net/http"
 )
 
@@ -11,7 +10,7 @@ Default web handler that gives a short descritption on how to use the api*/
 func DefaultHandler(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	default:
-		fmt.Fprintf(w, "To use the api use the following URL's \n"+constants.CASES_PATH+"\n"+
-			constants.POLICY_PATH+"\n"+constants.NOTIFICATION_PATH+"\n"+constants.STATUS_PATH)
+		http.Error(w, "To use the api use the following URL's \n"+constants.CASES_PATH+"\n"+
+			constants.POLICY_PATH+"\n"+constants.NOTIFICATION_PATH+"\n"+constants.STATUS_PATH, http.StatusAccepted)
 	}
 }
